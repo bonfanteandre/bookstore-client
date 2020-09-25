d5b18eb33a423db6fc296e5dc6aacaa7780dcf00
André Bonfante <andre458bonfante@gmail.com>
2020-09-25 09:38:42 -0300

    Service para obter endpoint do back-end

--- src/app/books/shared/services/book-service.ts
+++ src/app/books/shared/services/book-service.ts
@@ -2,33 +2,32 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Book } from "../models/book";
-
-const API = 'http://localhost:5000';
+import { EndpointService } from "src/app/core/services/endpoint.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class BooksService {
     
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private endpointService: EndpointService) { }
 
     public getAllBooksOrderedByName(): Observable<Book[]> {
-        return this.http.get<Book[]>(`${API}/books`);
+        return this.http.get<Book[]>(`${this.endpointService.getApiEndpoint()}/books`);
     }
 
     public find(id: string): Observable<Book> {
-        return this.http.get<Book>(`${API}/books/${id}`);
+        return this.http.get<Book>(`${this.endpointService.getApiEndpoint()}/books/${id}`);
     }
 
     public delete(book: Book): Observable<any> {
-        return this.http.delete(`${API}/books/${book.id}`);
+        return this.http.delete(`${this.endpointService.getApiEndpoint()}/books/${book.id}`);
     }
 
     public add(book: Book): Observable<any> {
-        return this.http.post(`${API}/books`, book);
+        return this.http.post(`${this.endpointService.getApiEndpoint()}/books`, book);
     }
 
     public update(book: Book): Observable<any> {
-        return this.http.put(`${API}/books/${book.id}`, book);
+        return this.http.put(`${this.endpointService.getApiEndpoint()}/books/${book.id}`, book);
     }
 }
