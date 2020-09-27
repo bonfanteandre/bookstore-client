c1df0f6b8775d30448471c9407cce7f7876767c2
André Bonfante <andre458bonfante@gmail.com>
2020-09-27 10:10:06 -0300

    Implementando sign out

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "../../user/user.service";
 
 @Component({
     selector: 'bs-header',
     templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+
+    constructor(private userService: UserService, private router: Router) { }
+
+    private signOut(): void {
+        let signOut = confirm('Tem certeza que deseja sair?');
+        if (signOut) {
+            this.userService.logout();
+            this.router.navigate(['sign-in']);
+        }
+    }
 
 }
